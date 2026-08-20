ffa9e9532fcfd587383213bc10ebbc73ee61b82d
Wesley Mazzinghy Tomáz <w.tomaz@aluno.senai.br>
2026-08-20 09:05:04 -0300

    alterações 20/08

--- src/ToDoListProfessor.java
+++ src/ToDoListProfessor.java
@@ -76,7 +76,24 @@ public class ToDoListProfessor {
                 }else{
                     System.out.println("Tarefa inválida");
                 }
+                break;
+            case 4:
+                System.out.println("Digite o número da tarefa que deseja excluir: ");
+                int numeroExcluir = scanner.nextInt();
                 
+                int indiceExcluir = numeroExcluir -1;
+                
+                if (indiceExcluir >= 0 && indiceExcluir < tarefas.length && tarefas[indiceExcluir] != null){
+                    
+                    tarefas[indiceExcluir] = null;
+                    concluidas[indiceExcluir] = false;
+                    
+                    System.out.println("Tarefa excluída com sucesso");
+                }else{
+                    System.out.println("Tarefa inválida");
+                    
+                }         
+                break;
             }
             
         }
